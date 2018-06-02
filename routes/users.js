67acb1d293af435498959788a10d5b8e479e5d07
MinchanKim <kimab7890@gmail.com>
2018-06-03 03:01:04 +0900

    user.js 로그인 로그아웃 메서드 추기

--- routes/users.js
+++ routes/users.js
@@ -6,22 +6,22 @@ const Coord = require('../models/coord');
 
 router.get('/', (req, res) => {
   User.find()
-  .then(users => {
-    res.status(200).json(users);
-  })
-  .catch(err => {
-    res.status(500).send({error: 'DB Failure'});
-  });
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(err => {
+      res.status(500).send({ error: 'DB Failure' });
+    });
 });
 
 router.get('/find/:_id', (req, res) => {
-  User.findOne({_id: req.params._id})
-  .then(users => {
-    res.status(200).json(users);
-  })
-  .catch(err => {
-    res.status(500).send({error: 'DB Failure'});
-  });
+  User.findOne({ _id: req.params._id })
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(err => {
+      res.status(500).send({ error: 'DB Failure' });
+    });
 });
 
 router.post('/', (req, res) => {
@@ -32,9 +32,9 @@ router.post('/', (req, res) => {
     if (err) return;
   });
 
-  Coord.findOne({lat: req.body.lat, lng: req.body.lng}, (err, coord) => {
-    if (err) return res.status(500).json({error: err});
-    if (!coord) return res.status(404).json({error: 'Not Found'});
+  Coord.findOne({ lat: req.body.lat, lng: req.body.lng }, (err, coord) => {
+    if (err) return res.status(500).json({ error: err });
+    if (!coord) return res.status(404).json({ error: 'Not Found' });
     let user = new User();
     user.id = req.body.id;
     user.password = req.body.password;
@@ -47,18 +47,18 @@ router.post('/', (req, res) => {
     user.save(err => {
       if (err) {
         console.error(err);
-        res.json({result: 0});
+        res.json({ result: 0 });
         return;
       }
-      res.json({result: 1});
+      res.json({ result: 1 });
     });
   });
 });
 
 router.put('/update/:_id', (req, res) => {
   User.findById(req.params._id, (err, users) => {
-    if (err) return res.status(500).json({error: err});
-    if (!URLSearchParams) return res.status(404).json({error: 'Not Found'});
+    if (err) return res.status(500).json({ error: err });
+    if (!URLSearchParams) return res.status(404).json({ error: 'Not Found' });
 
     if (req.body.id) user.id = req.body.id;
     if (req.body.password) user.password = req.body.password;
@@ -71,18 +71,75 @@ router.put('/update/:_id', (req, res) => {
     user.save(err => {
       if (err) {
         console.error(err);
-        res.json({result: 0});
+        res.json({ result: 0 });
         return;
       }
-      res.json({result: 1});
+      res.json({ result: 1 });
     });
   });
 });
 
 router.delete('/delete/:_id', (req, res) => {
-  User.remove({_id: req.params._id}, err => {
+  User.remove({ _id: req.params._id }, err => {
     if (err) res.status(500).end();
     res.status(204).end();
+  });
+});
+
+router.post('/login', (req, res) => {
+  let id = req.body.id;
+  let pw = req.body.password;
+
+  User.findOne({ id: id, password: pw })
+    .then(user => {
+      res.status(200).json(user._id);
+    })
+    .catch(err => {
+      res.status(500).json({ error: 'DB Failure' });
+    });
+});
+
+router.post('/register', (req, res) => {
+  let body = req.body;
+  let user = { };
+
+  User.findOne({id: req.body.id})
+    .then(() => {
+      res.status(200).json({message : "id was already taken"});
+    })
+    .catch(err => {
+      res.status(500).json({ error: 'DB Failure' });
+    });
+
+  if (req.body.id) user.id = req.body.id;
+  if (req.body.password) user.password = req.body.password;
+  if (req.body.name) user.name = req.body.name;
+  if (req.body.age) user.age = parseInt(req.body.age);
+  if (req.body.favorite) user.favorite = req.body.favorite;
+  if (req.body.introduce) user.introduce = req.body.introduce;
+  if (req.body.live) user.live = req.body.live;
+
+  if (req.body.id && req.body.password && req.body.name) {
+    res.status(200).json({error : 'Invalid input!'});
+  }
+
+  let _user = new User({
+    id : user.id,
+    password : user.password,
+    name : user.name,
+    age : user.age,
+    favorite : user.favorite,
+    introduce : user.introduce,
+    live : user.live
+  });
+
+  _user.save(err => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ result: 0 });
+      return;
+    }
+    res.json({ result: 1 });
   });
 });
 
